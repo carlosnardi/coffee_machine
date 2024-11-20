a7a553942ba26f8f8da15d6732a7768da1361476
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2024-11-20 11:58:21 +0000

    Adjusted to a concise code.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def payment():
   dimes = 0.10
   nickles = 0.05
   pennies = 0.01
+  print('Please insert coins.')
   quarters = float(input("How many quarters: ")) * quarters 
   dimes = float(input("How many dimes: ")) * dimes
   nickles = float(input("How many nickles: ")) * nickles
@@ -15,13 +16,21 @@ def payment():
   return total_payed
 
 def check_product(product):
-  if product == 'espresso':
-    return resources['water'] >= MENU[product]['ingredients']['water'] and resources['coffee'] >= MENU[product]['ingredients']['coffee']
-  else:
-    return resources['water'] >= MENU[product]['ingredients']['water'] and resources['coffee'] >= MENU[product]['ingredients']['coffee'] and resources['milk'] >= MENU[product]['ingredients']['milk'] 
+  for item in MENU[product]['ingredients']:
+      if MENU[product]['ingredients'][item] > resources[item]:
+          print(f'Not enough {item}')
+          return False
+  return True
+
+def make_product2(product):
+  '''A brief make coffee function'''
+  for item in MENU[product]['ingredients']:
+    resources[item] -= MENU[product]['ingredients'][item]
+  money['value'] += MENU[product]['cost']
+  print(f'Enjoy your {product.title()}!')
 
 def make_product(product):
-  # poderia mudar pra um for cada item do MENU
+  '''Not using anymore'''
   resources['water'] -= MENU[product]['ingredients']['water'] 
   resources['coffee'] -= MENU[product]['ingredients']['coffee']
   if product != 'espresso':
@@ -33,19 +42,21 @@ while True:
   menu = input("What would you like? (espresso/latte/cappuccino): ").lower()
   if menu == 'off':
     break
-  if menu == 'report':
+  elif menu == 'report':
     for i in resources:
       print(f"{i.title()}: {resources[i]}")
     print(f"Money: {money['value']}")
-  else:
+  elif menu == 'espresso' or menu == 'latte' or menu == 'cappuccino':
     if check_product(menu) == True:
       pay = payment()
       if pay >= MENU[menu]['cost']:
         money_change = pay - MENU[menu]['cost']
         if money_change > 0:
-          print(f'Here is your change: {money_change}')
-        make_product(menu)
+          print(f'Here is your change: {round(money_change,2)}')
+        make_product2(menu)
       else:
         print('Not enough money')
     else:
       print('Sorry. Not enough ingredients.')
+  else: 
+    print('Please choose again')
